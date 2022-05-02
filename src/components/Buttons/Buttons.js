5e8606350a1e83123aaf99a6fd0849c59f0b1ddc
PS <piotr.sroka@outlook.com>
2022-05-02 23:51:29 +0200

    add buttons style

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,7 +1,30 @@
 import React from "react";
+import style from './buttons.module.css'
 
 const Buttons = (params) => {
-    return 'test'
+    return (
+        <div className={style.buttonsBox}>
+            <div  className={style.button}>%</div>
+            <div  className={style.button}>+/-</div>
+            <div  className={style.button}>C</div>
+            <div  className={style.button}>/</div>
+            <div  className={style.button}>7</div>
+            <div  className={style.button}>8</div>
+            <div  className={style.button}>9</div>
+            <div  className={style.button}>x</div>
+            <div  className={style.button}>4</div>
+            <div  className={style.button}>5</div>
+            <div  className={style.button}>6</div>
+            <div  className={style.button}>-</div>
+            <div  className={style.button}>1</div>
+            <div  className={style.button}>2</div>
+            <div  className={style.button}>3</div>
+            <div  className={style.button}>+</div>
+            <div  className={style.button}>0</div>
+            <div  className={style.button}>,</div>
+            <div  className={style.button}>=</div>
+        </div>
+    )
 }
 
 
